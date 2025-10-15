4732a8ed3cdff1f9f3d2069aee7e8a933ebd988d
lethabo <shysnylethabo11@gmail.com>
2025-10-15 17:28:19 +0200

    Created button component

--- apps/native/app/(drawer)/index.tsx
+++ apps/native/app/(drawer)/index.tsx
@@ -1,19 +1,29 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { Container } from "@/components/container";
+import Button from "@/components/button";
 
 export default function Home() {
   return (
     <Container>
-      <ScrollView showsVerticalScrollIndicator={false} className="flex-1">
+      <ScrollView showsVerticalScrollIndicator={false} className="flex-1 p-3">
         <Text className="font-mono text-foreground text-3xl font-bold mb-4">
-          BRINKIFY
+          Welcome to Brinkify
+        </Text>
+        <Text>
+          South Africa's trusted platform for connecting skilled workers with
+          customers
         </Text>
         <View className="bg-card border border-border rounded-xl p-6 mb-6 shadow-sm">
-          <Text className="text-primary text-base">
+          <Text className="text-foreground text-base">
             This is a starter template for a React Native app using Expo and
             NativeWind. It includes a basic setup with a drawer navigation and a
             home screen.
           </Text>
+        </View>
+
+        <View>
+          <Button title="Create Account" />
+          <TouchableOpacity title="Login" onPress={() => {}} />
         </View>
       </ScrollView>
     </Container>
